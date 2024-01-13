6fa774ce5c056bfdd97c612c474e014465a471c9
Midnight <82400448+MCMidnight@users.noreply.github.com>
2024-01-13 09:53:01 +0000

    Create dllmain.cpp

---
+++ dllmain.cpp
@@ -0,0 +1,10 @@
+// dllmain.cpp : Define the entry point for the DLL application.
+#inlude "pch.h"
+
+BOOL APIENTRY DLLMain( HMODULE hModule,
+                       DWORD ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+
+}
